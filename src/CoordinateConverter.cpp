442d4d670a6c35bf32d55999752d3d1c5cfeacfc
Francisco Enriquez <Francisco.r.Enriquez@gmail.com>
2018-06-29 18:01:23 -0700

    Intial setup
    Initial setup of source files.

---
+++ src/CoordinateConverter.cpp
@@ -0,0 +1,26 @@
+/*
+ * CoordinateConverter.cpp
+ *
+ *  Created on: Jun 29, 2018
+ *      Author: Francisco
+ */
+
+#include "CoordinateConverter.h"
+
+namespace traxx {
+
+CoordinateConverter::CoordinateConverter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CoordinateConverter::~CoordinateConverter() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace traxx */
+
+int main() {
+
+	return 0;
+}
---
+++ src/CoordinateConverter.h
@@ -0,0 +1,21 @@
+/*
+ * CoordinateConverter.h
+ *
+ *  Created on: Jun 29, 2018
+ *      Author: Francisco
+ */
+
+#ifndef COORDINATECONVERTER_H_
+#define COORDINATECONVERTER_H_
+
+namespace traxx {
+
+class CoordinateConverter {
+public:
+	CoordinateConverter();
+	virtual ~CoordinateConverter();
+};
+
+} /* namespace traxx */
+
+#endif /* COORDINATECONVERTER_H_ */
